f3408a8b5afd44ced0aadc8920653975a1908ebb
Elad <elad3366@gmail.com>
2022-06-17 18:04:43 +0300

    upgraded tap-taboola to version 0.3.0: * using latest singer-python version (v5.12.2) and backoff (v1.8.0) * added schemas json with new schema support for campaign_hourly_performance * added discover and catalog * verify account access for multiple accounts under main account * added an ability to use already generated token

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup
 
 setup(name='tap-taboola',
-      version='0.2.1',
+      version='0.3.0',
       description='Singer.io tap for extracting data from the Taboola API',
       author='Fishtown Analytics',
       url='http://www.singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_taboola'],
       install_requires=[
-          'singer-python==5.0.4',
-          'backoff==1.3.2',
+          'singer-python==5.12.2',
+          'backoff==1.8.0',
           'requests==2.20.0',
           'python-dateutil==2.6.0'
       ],
@@ -19,5 +19,11 @@ setup(name='tap-taboola',
           [console_scripts]
           tap-taboola=tap_taboola:main
       ''',
-      packages=['tap_taboola']
-)
+      packages=['tap_taboola'],
+      package_data={
+          'tap_taboola/schemas': [
+              # add schema.json filenames here
+          ]
+      },
+      include_package_data=True
+      )
--- tap_taboola/__init__.py
+++ tap_taboola/__init__.py
@@ -1,33 +1,26 @@
 #!/usr/bin/env python3
-
-from decimal import Decimal
-
-import argparse
-import copy
 import datetime
-import json
 import os
-import sys
-import time
-import logging
-
-import dateutil.parser
-
-import singer
-from singer import utils
+import json
+from singer import Transformer, utils, metadata, get_logger, write_schema, write_record
+from singer.catalog import Catalog, CatalogEntry
+from singer.schema import Schema
 import requests
-
 import backoff
 
-import tap_taboola.schemas as schemas
-
-LOGGER = singer.get_logger()
-
+REQUIRED_CONFIG_KEYS = ['start_date', 'account_id']
+ACCESS_TOKEN_CONFIG_KEY = 'access_token'
+GENERATE_TOKEN_CONFIG_KEYS = ['username', 'password', 'client_id', 'client_secret']
 BASE_URL = 'https://backstage.taboola.com'
+LOGGER = get_logger()
+
+
+class TapTaboolaException(Exception):
+    pass
 
 
 @backoff.on_exception(backoff.expo,
-                      (requests.exceptions.RequestException),
+                      requests.exceptions.RequestException,
                       max_tries=5,
                       giveup=lambda e: e.response is not None and 400 <= e.response.status_code < 500, # pylint: disable=line-too-long
                       factor=2)
@@ -47,6 +40,7 @@ def request(url, access_token, params={}):
 
     response.raise_for_status()
     return response
+
 
 def get_token_password_auth(client_id, client_secret, username, password):
     url = '{}/backstage/oauth/token'.format(BASE_URL)
@@ -71,8 +65,11 @@ def get_token_password_auth(client_id, client_secret, username, password):
         result = {"token": response.json().get('access_token', None)}
     elif response.status_code >= 400 and response.status_code < 500:
         result = {k: response.json().get(k) for k in ('error','error_description')}
+    else:
+        result = {}
 
     return result
+
 
 def get_token_client_credentials_auth(client_id, client_secret):
     url = '{}/backstage/oauth/token'.format(BASE_URL)
@@ -93,8 +90,10 @@ def get_token_client_credentials_auth(client_id, client_secret):
     if response.status_code == 200:
         LOGGER.info("Got an access token.")
         result = {"token": response.json().get('access_token', None)}
-    elif response.status_code >= 400 and response.status_code < 500:
-        result = {k: response.json().get(k) for k in ('error','error_description')}
+    elif 400 <= response.status_code < 500:
+        result = {k: response.json().get(k) for k in ('error', 'error_description')}
+    else:
+        result = {}
 
     return result
 
@@ -114,132 +113,39 @@ def generate_token(client_id, client_secret, username, password):
 
     return token
 
-def parse_campaign_performance(campaign_performance):
-    return {
-        'campaign_id': int(campaign_performance.get('campaign')),
-        'impressions': int(campaign_performance.get('impressions', 0)),
-        'ctr': float(campaign_performance.get('ctr', 0.0)),
-        'cpc': float(campaign_performance.get('cpc', 0.0)),
-        'cpa_actions_num': int(campaign_performance.get('cpa_actions_num', 0)),
-        'cpa': float(campaign_performance.get('cpa', 0.0)),
-        'cpm': float(campaign_performance.get('cpm', 0.0)),
-        'clicks': int(campaign_performance.get('clicks', 0)),
-        'currency': str(campaign_performance.get('currency', '')),
-        'cpa_conversion_rate': float(campaign_performance.get(
-            'cpa_conversion_rate', 0.0)),
-        'spent': float(campaign_performance.get('spent', 0.0)),
-        'date': str(datetime.datetime.strptime(
-            campaign_performance.get('date'),
-            '%Y-%m-%d %H:%M:%S.%f'
-        ).date()),
-    }
 
-def fetch_campaign_performance(config, state, access_token, account_id):
-    url = ('{}/backstage/api/1.0/{}/reports/campaign-summary/dimensions/campaign_day_breakdown' #pylint: disable=line-too-long
-           .format(BASE_URL, account_id))
-
-    params = {
-        'start_date': state.get('start_date', config.get('start_date')),
-        'end_date': datetime.date.today(),
-    }
-
-    campaign_performance = request(url, access_token, params)
-    return campaign_performance.json().get('results')
-
-
-def sync_campaign_performance(config, state, access_token, account_id):
-    performance = fetch_campaign_performance(config, state, access_token,
-                                             account_id)
-
-    time_extracted = utils.now()
-
-    LOGGER.info("Got {} campaign performance records."
-                .format(len(performance)))
-
-    for record in performance:
-        parsed_performance = parse_campaign_performance(record)
-
-        singer.write_record('campaign_performance',
-                            parsed_performance,
-                            time_extracted=time_extracted)
-
-    LOGGER.info("Done syncing campaign_performance.")
-
-
-def parse_campaign(campaign):
-    start_date = campaign.get('start_date')
-    end_date = campaign.get('end_date')
-
-    return {
-        'id': int(campaign.get('id')),
-        'advertiser_id': str(campaign.get('advertiser_id', '')),
-        'name': str(campaign.get('name', '')),
-        'tracking_code': str(campaign.get('tracking_code', '')),
-        'cpc': float(campaign.get('cpc', 0.0)),
-        'daily_cap': float(campaign.get('daily_cap', 0.0)),
-        'spending_limit': float(campaign.get('spending_limit', 0.0)),
-        'spending_limit_model': str(campaign.get('spending_limit_model', '')),
-        'country_targeting': campaign.get('country_targeting'),
-        'platform_targeting': campaign.get('platform_targeting'),
-        'publisher_targeting': campaign.get('publisher_targeting'),
-        'start_date': str('9999-12-31' if start_date is None else start_date),
-        'end_date': str('9999-12-31' if end_date is None else end_date),
-        'approval_state': str(campaign.get('approval_state', '')),
-        'is_active': bool(campaign.get('is_active', False)),
-        'spent': float(campaign.get('spent', 0.0)),
-        'status': str(campaign.get('status', '')),
-    }
-
-def fetch_campaigns(access_token, account_id):
-    url = '{}/backstage/api/1.0/{}/campaigns/'.format(BASE_URL, account_id)
+def verify_account_access(config):
+    account_id = config.get('account_id')
+    access_token = config.get('access_token')
+    """
+    Fetch a list of current user's permitted accounts
+    See Backstage docs: Users Section 1.2.2
+    """
+    url = '{}/backstage/api/1.0/users/current/allowed-accounts/'.format(BASE_URL)
 
     response = request(url, access_token)
-    return response.json().get('results')
+    results = response.json().get('results')
+    for account in results:
+        if account['account_id'] == account_id and account.get('is_active') is True:
+            LOGGER.info("Verified account access via token details endpoint.")
+            return True
 
+    return False
 
-def sync_campaigns(access_token, account_id):
-    campaigns = fetch_campaigns(access_token, account_id)
-    time_extracted = utils.now()
-
-    LOGGER.info('Synced {} campaigns.'.format(len(campaigns)))
-
-    for record in campaigns:
-        parsed_campaigns = parse_campaign(record)
-
-        singer.write_record('campaigns',
-                            parsed_campaigns,
-                            time_extracted=time_extracted)
-
-    LOGGER.info("Done syncing campaigns.")
-
-
-def verify_account_access(access_token, account_id):
-    url = '{}/backstage/api/1.0/token-details/'.format(BASE_URL)
-
-    result = request(url, access_token)
-
-    token_account_id = result.json().get('account_id')
-    if token_account_id != account_id:
-        LOGGER.warn(("The provided `account_id` ({}) doesn't match the "
-                     "`account_id` of the token issued ({})").format(account_id, token_account_id))
-        return token_account_id
-
-    LOGGER.info("Verified account access via token details endpoint.")
-    return account_id
 
 def validate_config(config):
-    required_keys = ['username', 'password', 'account_id',
-                     'client_id', 'client_secret', 'start_date']
     missing_keys = []
     null_keys = []
     has_errors = False
 
-    for required_key in required_keys:
-        if required_key not in config:
-            missing_keys.append(required_key)
+    if not config.get(ACCESS_TOKEN_CONFIG_KEY):
+        LOGGER.info('Config is missing access token, checking for generate token keys')
+        for required_key in GENERATE_TOKEN_CONFIG_KEYS:
+            if required_key not in config:
+                missing_keys.append(required_key)
 
-        elif config.get(required_key) is None:
-            null_keys.append(required_key)
+            elif config.get(required_key) is None:
+                null_keys.append(required_key)
 
     if missing_keys:
         LOGGER.fatal("Config is missing keys: {}"
@@ -255,83 +161,165 @@ def validate_config(config):
         raise RuntimeError
 
 
-def load_config(filename):
-    config = {}
-
-    try:
-        with open(filename) as config_file:
-            config = json.load(config_file)
-    except:
-        LOGGER.fatal("Failed to decode config file. Is it valid json?")
-        raise RuntimeError
-
-    validate_config(config)
-
-    return config
+def get_abs_path(path):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
 
 
-def load_state(filename):
-    if filename is None:
-        return {}
+def load_schemas():
+    """ Load schemas from schemas folder """
+    schemas = {}
+    for filename in os.listdir(get_abs_path('schemas')):
+        path = get_abs_path('schemas') + '/' + filename
+        file_raw = filename.replace('.json', '')
+        with open(path) as file:
+            schemas[file_raw] = Schema.from_dict(json.load(file))
+    return schemas
 
-    try:
-        with open(filename) as state_file:
-            return json.load(state_file)
-    except:
-        LOGGER.fatal("Failed to decode state file. Is it valid json?")
-        raise RuntimeError
+
+def load_schema(stream):
+    path = get_abs_path('schemas/{}.json'.format(stream.tap_stream_id))
+    schema = utils.load_json(path)
+    return schema
 
 
-def do_sync(args):
-    LOGGER.info("Starting sync.")
+def initialize_stream(stream_id, schema):
+    """
+    :param stream_id:
+    :param schema:
+    :return: key_properties, stream_metadata
+    """
+    key_properties = []
+    stream_metadata = []
 
-    config = load_config(args.config)
-    state = load_state(args.state)
+    if stream_id == 'campaigns':
+        key_properties = ['id']
+        stream_metadata = metadata.get_standard_metadata(schema, key_properties=key_properties)
+    elif stream_id in ['campaign_day_performance', 'campaign_hourly_performance']:
+        key_properties = ['campaign']
+        stream_metadata = metadata.get_standard_metadata(schema, key_properties=key_properties)
 
-    access_token = generate_token(
-        client_id=config.get('client_id'),
-        client_secret=config.get('client_secret'),
-        username=config.get('username'),
-        password=config.get('password'))
+    return key_properties, stream_metadata
 
-    singer.write_schema('campaigns',
-                        schemas.campaign,
-                        key_properties=['id'])
 
-    singer.write_schema('campaign_performance',
-                        schemas.campaign_performance,
-                        key_properties=['campaign_id', 'date'])
+def discover():
+    raw_schemas = load_schemas()
+    streams = []
+    for stream_id, schema in raw_schemas.items():
+        key_properties, stream_metadata = initialize_stream(stream_id, schema.to_dict())
+        streams.append(
+            CatalogEntry(
+                tap_stream_id=stream_id,
+                stream=stream_id,
+                schema=schema,
+                key_properties=key_properties,
+                metadata=stream_metadata,
+                replication_key=None,
+                is_view=None,
+                database=None,
+                table=None,
+                row_count=None,
+                stream_alias=None
+            )
+        )
+    return Catalog(streams)
 
-    config['account_id'] = verify_account_access(access_token, config.get('account_id'))
 
-    sync_campaigns(access_token, config.get('account_id'))
-    sync_campaign_performance(config, state, access_token,
-                              config.get('account_id'))
+def fetch_campaigns(config):
+    account_id = config.get('account_id')
+    access_token = config.get('access_token')
 
-def main_impl():
-    parser = argparse.ArgumentParser()
+    url = '{}/backstage/api/1.0/{}/campaigns/'.format(BASE_URL, account_id)
 
-    parser.add_argument(
-        '-c', '--config', help='Config file', required=True)
-    parser.add_argument(
-        '-s', '--state', help='State file')
+    response = request(url, access_token)
+    return response.json().get('results')
 
-    args = parser.parse_args()
 
-    try:
-        do_sync(args)
-    except RuntimeError:
-        LOGGER.fatal("Run failed.")
-        exit(1)
+def fetch_campaign_day_performance(config, state):
+    account_id = config.get('account_id')
+    access_token = config.get('access_token')
 
+    url = (
+        '{}/backstage/api/1.0/{}/reports/campaign-summary/dimensions/campaign_day_breakdown'  # pylint: disable=line-too-long
+        .format(BASE_URL, account_id))
+
+    params = {
+        'start_date': state.get('start_date', config.get('start_date')),
+        'end_date': config.get('end_date', datetime.datetime.utcnow().date()),
+    }
+
+    campaign_performance = request(url, access_token, params)
+    return campaign_performance.json().get('results')
+
+
+def fetch_campaign_hourly_performance(config, state):
+    account_id = config.get('account_id')
+    access_token = config.get('access_token')
+
+    url = (
+        '{}/backstage/api/1.0/{}/reports/campaign-summary/dimensions/campaign_hour_breakdown'  # pylint: disable=line-too-long
+        .format(BASE_URL, account_id))
+
+    params = {
+        'start_date': state.get('start_date', config.get('start_date')),
+        'end_date': config.get('end_date', datetime.datetime.utcnow().date()),
+    }
+
+    campaign_performance = request(url, access_token, params)
+    return campaign_performance.json().get('results')
+
+
+def sync(config, state, catalog):
+    """ Sync data from tap source """
+    if not verify_account_access(config):
+        raise TapTaboolaException(f'Could not verify account id: {config["account_id"]}')
+    # Loop over selected streams in catalog
+    for stream in catalog.get_selected_streams(state):
+        LOGGER.info("Syncing stream:" + stream.tap_stream_id)
+
+        schema = load_schema(stream)
+        map_metadata = metadata.to_map(stream.metadata)
+        write_schema(
+            stream_name=stream.tap_stream_id,
+            schema=schema,
+            key_properties=stream.key_properties,
+        )
+
+        if stream.tap_stream_id == 'campaigns':
+            results = fetch_campaigns(config)
+        elif stream.tap_stream_id == 'campaign_day_performance':
+            results = fetch_campaign_day_performance(config, state)
+        elif stream.tap_stream_id == 'campaign_hourly_performance':
+            results = fetch_campaign_hourly_performance(config, state)
+        else:
+            raise TapTaboolaException('Unknown stream {}'.format(stream.tap_stream_id))
+
+        time_extracted = utils.now()
+        with Transformer() as transformer:
+            for result in results:
+                record = transformer.transform(result, schema, metadata=map_metadata)
+                write_record(stream.tap_stream_id, record, stream.stream_alias, time_extracted)
+
+
+@utils.handle_top_exception(LOGGER)
 def main():
-    try:
-        main_impl()
-    except Exception as exc:
-        LOGGER.critical(exc)
-        raise exc
+    # Parse command line arguments
+    args = utils.parse_args(REQUIRED_CONFIG_KEYS)
+    config = args.config
+    validate_config(config)
+    if not config.get('access_token'):
+        config['access_token'] = generate_token(config['client_id'], config['client_secret'], config['username'],
+                                                config['password'])
+
+    # If discover flag was passed, run discovery mode and dump output to stdout
+    if args.discover:
+        catalog = discover()
+        catalog.dump()
+    elif args.catalog:
+        catalog = args.catalog
+        sync(args.config, args.state, catalog)
+    else:
+        LOGGER.critical("No catalog were selected")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
